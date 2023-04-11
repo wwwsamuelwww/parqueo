a2a0153d8ddf79682e84f456404f817cb3e4fbba
LimbertQ <102129028+LimbertQ@users.noreply.github.com>
2023-04-11 00:35:44 -0400

    funciones corregidas

--- Backend/parqueo_act.php
+++ Backend/parqueo_act.php
@@ -1,26 +1,17 @@
 <?php
 
-//include './conectar.php';
+
+include_once '../Backend/conectar.php';
 
 
 ///NO ME FUNCIONA EL INCLUDE POR ESO ESTA AQUI LA CONEXION Y TAMPOCO FUNCA
-function conexion(){
-    $conexion=mysqli_connect('db4free.net','condor','betaalfa800','parqueo','3306');
-    if($conexion){
-        //return mysqli_query($conexion,'SET NAMES uff8');
-        return $conexion;
-    }
-    else {
-        return 'Error de conexion';
-    }
-}
 
 function hor_mod( $id_hro, $hro_at, $hro_ce, $id_prq, $lun, $mar, $mie, $jue, $vie, $sab)
 {
     $conn = conexion();
-    $sql = "UPDATE horario_parqueo (hora_entrada, hora_salida, parqueo_id, lun, mar, mie, jue, vie, sab) values 
-                                   ('$hro_at','$hro_ce','$id_prq','$lun','$mar','$mie','$jue','$vie','$sab')
-                                   WHERE id=$id_hro;";  
+    $sql = "UPDATE horario_estacionamiento  set hora_entrada = '$hro_at', hora_salida = '$hro_ce', estacionamiento_id = '$id_prq',
+                                                lun = '$lun', mar = '$mar', mie = '$mie', jue = '$jue', vie = '$vie', sab = '$sab'
+                                            WHERE id='$id_hro';";  
 
     $query = mysqli_query($conn,$sql);
     if(!$query){
@@ -34,7 +25,7 @@ function hor_mod( $id_hro, $hro_at, $hro_ce, $id_prq, $lun, $mar, $mie, $jue, $v
 function prq_mod( $id_prq, $nombre, $cantidad)
 {
     $conn = conexion();
-    $sql = "UPDATE parqueo (nombre, sitios) values ('$nombre','$cantidad') WHERE id=$id_prq;";  
+    $sql = "UPDATE estacionamiento set nombre = '$nombre', sitios = '$cantidad' WHERE id=$id_prq;";  
 
     $query = mysqli_query($conn,$sql);
     if(!$query){
@@ -48,7 +39,7 @@ function prq_mod( $id_prq, $nombre, $cantidad)
 function hor_reg( $id_hro, $hro_at, $hro_ce, $id_prq, $lun, $mar, $mie, $jue, $vie, $sab)
 {
     $conn = conexion();
-    $sql = "INSERT INTO horario_parqueo (id, hora_entrada, hora_salida, parqueo_id, lun, mar, mie, jue, vie, sab) values 
+    $sql = "INSERT INTO horario_estacionamiento (id, hora_entrada, hora_salida, estacionamiento_id, lun, mar, mie, jue, vie, sab) values 
                                    ('$id_hro','$hro_at','$hro_ce','$id_prq','$lun','$mar','$mie','$jue','$vie','$sab');";  
 
     $query = mysqli_query($conn,$sql);
@@ -63,7 +54,7 @@ function hor_reg( $id_hro, $hro_at, $hro_ce, $id_prq, $lun, $mar, $mie, $jue, $v
 function prq_reg( $id_prq, $nombre, $cantidad)
 {
     $conn = conexion();
-    $sql = "INSERT INTO parqueo (id, nombre, sitios) values 
+    $sql = "INSERT INTO estacionamiento (id, nombre, sitios) values 
                                    ('$id_prq','$nombre','$cantidad');";  
 
     $query = mysqli_query($conn,$sql);
@@ -78,10 +69,10 @@ function prq_reg( $id_prq, $nombre, $cantidad)
 function prq_vista($idP){
     $conn = conexion();
     $sql="SELECT p.nombre,p.sitios,h.hora_entrada,h.hora_salida,h.lun,h.mar,h.mie,h.jue,h.vie,h.sab
-    FROM parqueo p
-    JOIN horario_parqueo h
-    ON p.id =h.parqueo_id
-    WHERE parqueo_id=$idP";
+    FROM estacionamiento p
+    JOIN horario_estacionamiento h
+    ON p.id =h.id
+    WHERE estacionamiento_id=$idP";
 
     $query = mysqli_query($conn,$sql);
     if(!$query){
@@ -93,18 +84,4 @@ function prq_vista($idP){
 }
 
 
-/*
-function prq_mod( $id, $nombre, $precio, $cantidad, $hro_at, $hro_ce, $lun, $mar, $mie, $jue, $vie, $sab)
-{
-    $conn = conexion();
-    $sql = "INSERT INTO horario_parqueo (hora_entrada, hora_salida, parqueo_id, lun, mar, mie, jue, vie, sab) values 
-                                   ('$nombre','$precio ','$cantidad','$hro_at','$hro_ce','$lun','$mar','$mie','$jue','$vie','$sab');
-";  
-
-    $query = mysqli_query($conn,$sql);
-    if(!$query){
-        echo "algo salio mal";
-        exit;
-    }
-}*/
 ?>
