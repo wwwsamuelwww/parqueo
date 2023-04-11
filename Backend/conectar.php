1457102c841b766f8eedb09466907448e67fd326
LimbertQ <102129028+LimbertQ@users.noreply.github.com>
2023-04-11 00:34:11 -0400

    conexion corregida

--- Backend/conectar.php
+++ Backend/conectar.php
@@ -1,21 +1,6 @@
 <?php
 
-    /*
-    $host = 'db4free.net:3306'; 
-    $dbuser = 'condor';
-    $dbpassword = 'betaalfa800';
-    $dbname = 'parqueo';
-
-    $conn = mysqli_connect($host,$dbuser,$dbpassword,$dbname);
-    if($conn){
-        mysqli_query($conn,'SET NAMES uff8');
-        
-    }
-    else {
-        
-    }*/
-
-    function conexions(){
+    function conexion(){
         $conexion=mysqli_connect('db4free.net','condor','betaalfa800','parqueo','3306');
         if($conexion){
             //return mysqli_query($conexion,'SET NAMES uff8');
@@ -25,7 +10,5 @@
             return 'Error de conexion';
         }
     }
-
-
 ?>
 
