7a1e2ac279e788d06cbd78863e0f9f9d2cc98198
wwwsamuelwww <samuelspeed574@gmail.com>
2023-04-11 22:08:09 -0400

    eliminar

---
+++ Backend/eliminarEstacionamiento.php
@@ -0,0 +1,8 @@
+<?php
+    include_once '../Backend/conectar.php';
+
+    if(isset($_GET['borrar'])){
+        $var = $_GET['borrar'];
+        
+    }
+?>
--- Frontend/index.php
+++ Frontend/index.php
@@ -16,7 +16,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
+    
     <!-- Styles -->
     <link rel="stylesheet" href="assets/css/style.css">
 
@@ -101,7 +101,7 @@
                         <tbody>
                             <?php while($ver = mysqli_fetch_row($result)): ?>
                             <tr class="">
-                                <td><a href= "eliminarEstacionamiento.php?idEstacionamiento = <?php echo $ver[0];?>" style='text-decoration:none;color:black;' > <?php echo $ver[1]; ?> </a></td>
+                                <td><a href= "informacion.php?idEstacionamiento = <?php echo $ver[0];?>" style='text-decoration:none;color:black;' > <?php echo $ver[1]; ?> </a></td>
                                 <td><?php 
                                     $dias = "";
                                     if($ver[4] == true){
@@ -110,23 +110,46 @@
                                     if($ver[5] == true){
                                         $dias = $dias."Mar,"; 
                                     }
-                                    if($ver[4] == true){
+                                    if($ver[6] == true){
                                         $dias = $dias."Mié,"; 
                                     }
-                                    if($ver[4] == true){
+                                    if($ver[7] == true){
                                         $dias = $dias."Jue,"; 
                                     }
-                                    if($ver[4] == true){
+                                    if($ver[8] == true){
                                         $dias = $dias."Vie,"; 
                                     }
-                                    if($ver[4] == true){
+                                    if($ver[9] == true){
                                         $dias = $dias."Sab,"; 
                                     }
                                     $dias = substr($dias, 0, -1);
                                     echo $dias." ".substr( $ver[2], 0, 5 )."-".substr( $ver[3], 0, 5 ); 
                                 ?></td>
-                                <td style="text-align: center;"> 
-                                    <a class="btn btn-danger" href="?borrar=<?php echo $proyecto['id']; ?>" >Eliminar</a> 
+                                <td style="text-align: center;">
+                                <!-- Button trigger modal -->
+                                    <!-- Button trigger modal -->
+                                    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
+                                             Eliminar
+                                    </button>
+                                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLabel">Advertencia</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                Esta seguro de eliminar el estacionamiento?
+                                            </div>
+                                            <div class="modal-footer">
+                                                <a href= "eliminarEstacionamiento.php?borrar=<?php echo $ver[0]; ?>">Aceptar</a>
+                                                <a href= "index.php">Cerrar</a>                                            
+                                            </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                     <a class="btn btn-primary" href="?modificar=<?php echo $proyecto['id']; ?>" >Modificar</a> 
                                 </td>
                             </tr>
@@ -137,6 +160,20 @@
             </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js" integrity="sha256-R4pqcOYV8lt7snxMQO/HSbVCFRPMdrhAFMH+vr9giYI=" crossorigin="anonymous"></script>
+    <script src="Frontend/js/sidebars.js"></script>
+
+    
+    
     <script src="Frontend/js/sidebars.js"></script>
 </body>
 
